aee178ef4529a14914b64e3074edddeb4ffc35df
Rolan111 <97992969+Rolan111@users.noreply.github.com>
2023-07-10 11:51:14 -0500

    actualización

--- src/main/java/com/ucc/ova/OVAContaduriaApi/model/DataModel.java
+++ src/main/java/com/ucc/ova/OVAContaduriaApi/model/DataModel.java
@@ -8,12 +8,18 @@ public class DataModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id;
+    public int id_data;
 
     @Column(name = "id_usuario")
     public int idUsuario;
 
+    @Column(name = "json_caratula")
+    public String jsonCaratula;
+
     @Column(name = "json_patrimonio")
     public String jsonPatrimonio;
+
+    @Column(name = "json_renta_liquida")
+    public String jsonRentaLiquida;
 
 }
